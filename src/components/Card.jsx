0b8d73360a207656d319cf7f1d4b562c2e70eef2
Your Name <Sanchit Pandey>
2023-10-26 02:33:00 +0545

    text-sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,9 +7,9 @@ function Card({img,title,price,id,desc}) {
         <img className=" p-2 h-60 w-60 rounded-2xl" src={img} alt="product image" />
     </a>
     <div className="p-4">
-        <a href="#" className='flex justify-between flex-col'>
-            <h5 className="text-base font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-            <span className="font-extrabold text-red-900 dark:text-white">Rs {price}</span>
+        <a href="#" className='flex justify-between flex-col '>
+            <h5 className="text-base font-black tracking-tight text-gray-900 dark:text-white">{title}</h5>
+            <span className="font-extrabold text-sm mt-3 mb-3 text-red-900 dark:text-white">Rs {price}</span>
         </a>
         
         <div className="flex flex-col items-left justify-between">
