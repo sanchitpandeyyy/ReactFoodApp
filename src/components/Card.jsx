f3b0eefa057e944b5f84f0eb4a4f81e51a6ef126
Your Name <Sanchit Pandey>
2023-10-26 02:25:25 +0545

    search

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Card({img,title,price,id,desc}) {
   return (
-    <div className="w-60 border border-gray-200 rounded-lg shadow-xl bg-yellow-500" id={id}>
+    <div className="flex flex-col justify-between w-60 border border-gray-200 rounded-lg shadow-2xl bg-yellow-500" id={id}>
     <a href="#">
-        <img className=" p-2 h-64 w-64 rounded" src={img} alt="product image" />
+        <img className=" p-2 h-60 w-60 rounded-2xl" src={img} alt="product image" />
     </a>
     <div className="p-4">
-        <a href="#" className='flex justify-between'>
-            <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-            <span className="font-extrabold text-red-900 dark:text-white">${price}</span>
+        <a href="#" className='flex justify-between flex-col'>
+            <h5 className="text-base font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+            <span className="font-extrabold text-red-900 dark:text-white">Rs {price}</span>
         </a>
-        <p>{desc.slice(0,30)}...</p>
+        
         <div className="flex flex-col items-left justify-between">
             
             <a href="#" className="mt-3 w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,13 @@ import slider1 from '../img/slider1.webp';
 import slider2 from '../img/slider2.webp';
 import slider3 from '../img/slider3.webp';
 import Card from '../components/Card';
-import FoodData from '../data/FoodData'
+import FoodData from '../data/FoodData';
+import search from '../img/search.png';
 
 const Home = () => {
     const [datas, setDatas] = useState(FoodData)
     return (
-        <div className='p-10'>
+        <div className='p-10 dark:bg-gray-800'>
 
             <div id="default-carousel" className="relative w-full bg-blue-500 " data-carousel="slide">
 
@@ -61,7 +62,25 @@ const Home = () => {
                 </button>
             </div>
 
-            <div className="flex flex-wrap gap-6 w-[90%] m-auto mt-8">
+     
+
+ 
+
+        <form className=' mt-4'>   
+     <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"  fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round"  stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 " placeholder="Search Here" />
+        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+      </div>
+     </form>
+
+
+            <div className="flex justify-center flex-wrap gap-6 w-[90%] m-auto mt-8">
                 {
                     datas.map((data) => (
                         <Card
@@ -80,5 +99,6 @@ const Home = () => {
         </div>
     )
 }
+
 
 export default Home
