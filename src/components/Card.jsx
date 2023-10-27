4c2158a75e0abb55141d60179c180a7260bafea0
Your Name <Sanchit Pandey>
2023-10-28 00:45:02 +0545

    addtocart popup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {useState} from 'react';
+import Popup from './Popup';
 
 function Card({img,title,price,id}) {
 
 
   return (
-    <div  className="flex flex-col justify-between w-60 border border-gray-200 rounded-lg shadow-2xl bg-yellow-500" id={id}>
+    <div  className="flex flex-col justify-between w-60 border  rounded-2xl shadow-2xl bg-slate-500 " id={id}>
     <a href="#">
         <img className=" p-2 h-60 w-60 rounded-2xl" src={img} alt="product image" />
     </a>
@@ -16,9 +16,9 @@ function Card({img,title,price,id}) {
         </a>
         
         <div className="flex flex-col items-left justify-between">
-            
-            <a href="#" className="mt-3 w-32 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+        <Popup/>     
         </div>
+
     </div>
 </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
             </svg>
         </div>
         <input onChange={(e)=> setSearch(e.target.value)} type="search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  " placeholder="Search Here" />
-        <button  type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        
       </div>
      </form>
 
@@ -44,7 +44,7 @@ const Home = () => {
                             title={data.name}
                             price={data.price}
                             img={data.img}
-                            desc = {data.desc}
+    
                         />
 
                     ))
