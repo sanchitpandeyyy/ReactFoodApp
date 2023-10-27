895a263b987e53bad17d48497fa4659efa8ba05d
Your Name <Sanchit Pandey>
2023-10-28 00:50:23 +0545

    Navbar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
-import Logo from '../img/Logo.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Logo from "../img/Logo.png";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='flex justify-around items-center h-24 bg-yellow-500'>
-       <img src={Logo} alt='' className='w-20 h-20'/>
-       <div className='flex justify-around items-center w-full font-semibold'>
-         <Link to={'/'}>Home</Link>
-         <Link to={'/about'}>About</Link>
-         <Link to={'/contact'}>Contact</Link>
-         <Link to={'/cart'}>Cart</Link>
-       </div>
+    <div className="flex justify-between items-center h-24 bg-yellow-500">
+      <img src={Logo} alt="" className="w-20 h-20 ml-8" />
+      <div className="flex gap-8 items-center font-semibold text-lg">
+        <Link to={"/"} className="hover:opacity-50 hover:transition-all active:opacity-50 ">Home</Link>
+        <Link className="hover:opacity-50 hover:transition-all active:opacity-50 " to={"/about"}>About</Link>
+        <Link className="hover:opacity-50 hover:transition-all active:opacity-50 " to={"/contact"}>Contact</Link>
+      </div>
+      <div className="flex items-center gap-4 mr-8">
+        <Link
+          to={"/cart"}
+          className="font-medium text-lg bg-white p-4 rounded-full hover:scale-110 "
+        >
+          {" "}
+          <FaShoppingCart />{" "}
+        </Link>
+        <Link to={"/login"}>
+          {" "}
+          <button
+           className="bg-blue-700 text-white py-2 px-4 rounded-lg  hover:bg-white hover:text-black">
+            Login</button>{" "}
+        </Link>
+        <Link to={"/sign-up"}>
+          {" "}
+          <button className="bg-white text-black py-2 px-4 rounded-lg hover:bg-blue-700 hover:text-white hover:border-none">Sign Up</button>{" "}
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
